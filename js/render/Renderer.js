0d229a8dece4c20dac38a930d426a4b8967cb97e
Jason Chionh <jasonchionh@gmail.com>
2013-02-21 23:37:35 -0800

    after init of our shaders we extract handles to our attributes and uniforms

--- js/render/Renderer.js
+++ js/render/Renderer.js
@@ -13,9 +13,15 @@ wa01.render = wa01.render || {};
  * @constructor
  */
 wa01.render.Renderer = function(gl) {
+    // constants
+    this.MAX_NUM_RENDER_OBJECTS = 100;
+
+    // members
     this.gl = gl;
     // our shaderhandle refs sturcture
     this.shaderHandleRefs = new wa01.render.ShaderHandleRefs();
+    this.currentRenderObjectIndex = 0;
+    this.lastRenderObjectIndex = 0;
 }
 
 /**
@@ -31,5 +37,20 @@ wa01.render.Renderer.prototype.initDefaultShaders = function(vtxShaderId, fragSh
     var attribNames = ["a_Position", "a_Color", "a_TexCoord"];
     var shaderProgram = wa01.utils.createShaderProgram(this.gl, vtxShader, fragShader, attribNames);
     // assign to tour shader refs
-    this.shaderHandleRefs.programHandle = shaderProgram;
+    this.shaderHandleRefs.shaderProgram = shaderProgram;
+    // get our arrtibute handles
+    this.shaderHandleRefs.posHandle = this.gl.getAttribLocation(shaderProgram, "a_Position");
+    this.shaderHandleRefs.colorHandle = this.gl.getAttribLocation(shaderProgram, "a_Color");
+    this.shaderHandleRefs.texCoordHandle = this.gl.getAttribLocation(shaderProgram, "a_TexCoord");
+    this.shaderHandleRefs.matrixHandle = this.gl.getUniformLocation(shaderProgram, "u_MVPMatrix");
+    this.shaderHandleRefs.texMatrixHandle = this.gl.getUniformLocation(shaderProgram, "u_TexMatrix");
+    this.shaderHandleRefs.texSamplerHandle = this.gl.getUniformLocation(shaderProgram, "u_Texture");
+    //console.log("texSamplerHandle: " + this.shaderHandleRefs.texSamplerHandle);
+}
+
+/**
+ * here is our render function that iterates over our render objects and render them
+ */
+wa01.render.Renderer.prototyp.render = function() {
+
 }
--- js/render/ShaderHandleRefs.js
+++ js/render/ShaderHandleRefs.js
@@ -11,11 +11,11 @@ wa01.render = wa01.render || {};
  * @constructor
  */
 wa01.render.ShaderHandleRefs = function() {
-    this.programHandle = 0;
-    this.posHandle = 0;
-    this.colorHandle = 0;
-    this.matrixHandle = 0;
-    this.texMatrixHandle = 0;
-    this.texSamplerHandle = 0;
-    this.texCoordHandle = 0;
+    this.shaderProgram = null; // WebGLProgram
+    this.posHandle = 0; // GLint
+    this.colorHandle = 0; // GLint
+    this.matrixHandle = null; // WebGLUniformLocation
+    this.texMatrixHandle = null; // WebGLUniformLocation
+    this.texSamplerHandle = null; // WebGLUniformLocation
+    this.texCoordHandle = 0; // GLint
 }
