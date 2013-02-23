06fc1295c90060df128bb6b14891347a8b4962d9
Jason Chionh <jasonchionh@gmail.com>
2013-02-22 20:44:12 -0800

    adding a console log to indicate if requestAnimationFrame has fallback to setTimeout.

--- js/utils/webgl-utils.js
+++ js/utils/webgl-utils.js
@@ -157,6 +157,7 @@ window.requestAnimFrame = (function() {
          window.oRequestAnimationFrame ||
          window.msRequestAnimationFrame ||
          function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+           console.log("Falling back from requestAnimationFrame to setTimeout!");
            return window.setTimeout(callback, 1000/60);
          };
 })();
