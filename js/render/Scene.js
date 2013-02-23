1029b732dabd6f7eb00ac7fff2421c6e89192a98
Jason Chionh <jasonchionh@gmail.com>
2013-02-23 04:07:52 -0800

    new scene class

---
+++ js/render/Scene.js
@@ -0,0 +1,15 @@
+/**
+ * User: jchionh
+ * Date: 2/23/13
+ * Time: 4:05 AM
+ */
+
+// namespace
+wa01.render = wa01.render || {};
+
+/**
+ * Scene is a structure that has intrusive lists of scene nodes
+ * @constructor
+ */
+wa01.render.Scene = function() {
+}
