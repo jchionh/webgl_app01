61328e5d3bbf3086b3881cb773fcabd95e9c0168
Jason Chionh <jasonchionh@gmail.com>
2013-02-23 20:50:33 -0800

    added transforms to scene node

--- js/render/SceneNode.js
+++ js/render/SceneNode.js
@@ -22,6 +22,24 @@ wa.render.SceneNode = function() {
     wa.utils.IntrusiveListNode.call(this);
 
     // now here's our transforms
+    this.position = vec3.create();
+    this.scale = vec3.create();
+    this.orientation = vec3.create();
+
+    // here's our texture transforms
+    // they are all 2D transforms, but we use vec3 simply
+    // because that is what we have in our matrix library
+    this.texTranslate = vec3.create();
+    this.texRotate = vec3.create();
+    this.texScale = vec3.create();
+
+    // and here's our transform
+    this.modelMatrix = mat4.create();
+    this.texMatrix = mat4.create();
+
+    // indentity our matrices
+    mat4.identity(this.modelMatrix);
+    mat4.identity(this.texMatrix);
 };
 
 // perform prototype extend
