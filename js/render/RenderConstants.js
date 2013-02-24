8823981e6b5ea253da66612f47766c05bcf13806
Jason Chionh <jasonchionh@gmail.com>
2013-02-23 21:46:17 -0800

    shape base class

--- js/render/RenderConstants.js
+++ js/render/RenderConstants.js
@@ -3,7 +3,15 @@
  * Date: 2/23/13
  * Time: 8:40 PM
  */
+// namespace
+wa.render = wa.render || {};
+// static object for constants
+wa.render.RenderConstants = {};
 
+wa.render.RenderConstants.FLOATS_PER_VTX = 3;
+wa.render.RenderConstants.FLOATS_PER_TEX_COORD = 2;
+
+// these are in sort form for ease
 var v = {};
 v.X = 0;
 v.Y = 1;
--- js/render/Shape.js
+++ js/render/Shape.js
@@ -3,3 +3,76 @@
  * Date: 2/23/13
  * Time: 9:05 PM
  */
+// namespace
+wa.render = wa.render || {};
+
+/**
+ * Shape contains the information to define a 3D shape
+ * - the vertex buffers
+ * - the texture coordinate buffers
+ * - the vertex colors
+ * @constructor
+ */
+wa.render.Shape = function() {
+    this.vertices = null;
+    this.vtxColors = null;
+    this.texCoords = null;
+    this.numVertices = 0;
+};
+
+/**
+ *
+ * @param {Array.<number>} vertexArray
+ */
+wa.render.Shape.prototype.initVertices = function(vertexArray) {
+    this.vertices = new Float32Array(vertexArray);
+    this.numVertices = vertexArray.length / wa.render.RenderConstants.FLOATS_PER_VTX;
+};
+
+/**
+ *
+ * @param {Array.<number>} vtxColorArray
+ */
+wa.render.Shape.prototype.initVtxColors = function(vtxColorArray) {
+    this.vtxColors = new Float32Array(vtxColorArray);
+};
+
+/**
+ *
+ * @param {Array.<number>} texCoordArray
+ */
+wa.render.Shape.prototype.initTexCoords = function(texCoordArray) {
+    this.texCoords = new Float32Array(texCoordArray);
+};
+
+/**
+ *
+ * @return {number} the number of vertices
+ */
+wa.render.Shape.prototype.getNumVertices = function() {
+    return this.numVertices;
+};
+
+/**
+ *
+ * @return {Float32Array} vertex array
+ */
+wa.render.Shape.prototype.getVertices = function() {
+    return this.vertices;
+};
+
+/**
+ *
+ * @return {Float32Array} the vertex colors
+ */
+wa.render.Shape.prototype.getVtxColors = function() {
+    return this.vtxColors;
+};
+
+/**
+ *
+ * @return {Float32Array} the texture coords
+ */
+wa.render.Shape.prototype.getTexCoords = function() {
+    return this.texCoords;
+};
