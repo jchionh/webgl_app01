98a9e0868da0bd1e5f232689d3ae074831823ae8
Jason Chionh <jasonchionh@gmail.com>
2013-02-27 00:40:48 -0800

    use bilinear filtering for min and mag filters
    - also updated notes about re-sampling images and generating mipmaps

--- js/cache/TextureLibrary.js
+++ js/cache/TextureLibrary.js
@@ -56,8 +56,8 @@ wa.cache.TextureLibrary.prototype.addTexture = function(image) {
     // Set the parameters so we can render any size image.
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
     this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
-    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
     this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGBA, this.gl.RGBA, this.gl.UNSIGNED_BYTE, image);
     // create the texture object
     texture = new wa.texture.Texture();
